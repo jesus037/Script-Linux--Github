353185c05870c42fa2cd757a9c33db82381e847d
jesus037 <58068481+jesus037@users.noreply.github.com>
2021-04-26 20:04:41 +0200

    Create Script 4.sh

---
+++ Script 4.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#Ejemplo, que podemos hacer que nos informe sobre el contenido de un directorio
+	
+  for file in $(ls);
+	do
+	  if [ -d $file ];
+	  then
+	    echo "directorio:" $file
+	  else
+	    if [ -x $file ];
+	    then
+	      echo "archivo ejecutable:" $file
+	    else
+	      echo "archivo no ejecutable:" $file
+	    fi
+	  fi
+	done
